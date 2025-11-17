699324296e126f0f98927ee3c11fb157655ff15a
asthwuisan <asthriedwuisan09@gmail.com>
2025-11-17 11:35:38 +0800

    update splashscreen

---
+++ src/SplashScreen.tsx
@@ -0,0 +1,36 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React, {useEffect} from 'react';
+import Logo from '../assets/logo.png';
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('OnboardingScreen'); // Pindah otomatis
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.logo} />
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#B6CF7B',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 412,
+    height: 410,
+    marginBottom: 12,
+    resizeMode: 'contain',
+  },
+});
